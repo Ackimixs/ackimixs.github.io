d9996fc3c24b962a256eea2907afa39fec7c888d
Ackimixs <guichetkillian@gmail.com>
2021-12-08 13:23:31 +0100

    my name

--- easter egg/script.js
+++ easter egg/script.js
@@ -1,20 +1,30 @@
 function watchTextbox() {
-    var txtInput = $('#txtInput');
-    var lastValue = txtInput.data('lastValue');
-    var currentValue = txtInput.val();
-    if (lastValue != currentValue) {
-      console.log('Value changed from ' + lastValue + ' to ' + currentValue);
-      txtInput.data('lastValue', currentValue);
+    var option1 = 'guichet killian';
+    var option2 = 'Guichet Killian';
+    var option3 = 'GUICHET Killian';
+    var option4 = 'GUICHET KILLIAN';
+    var option5 = 'GUICHET killian';
+    var option6 = 'Killian Guichet';
+    var option7 = 'killian guichet';
+    var option8 = 'Killian GUICHET';
+
+
+
+    var txtInput = $('#txtInput').val();
+    if (txtInput == option1 || txtInput == option2 || txtInput == option3 || txtInput == option4 || txtInput == option5 || txtInput == option6 || txtInput == option7 || txtInput == option8) {
+      console.log('That normally work');
+      document.getElementById('Secret').innerHTML = "You can't this is me";   // when it's me it's not me
+      document.getElementById("submit").disabled = true;
+    }
+    else {
+      document.getElementById('Secret').innerHTML = null;  // When it's not me it's not me
+      document.getElementById("submit").disabled = false;
     }
   }
-
   // Record the initial value of the textbox.
   $('#txtInput').data('lastValue', $('#txtInput').val());
 
   // Bind to the keypress and user-defined set event.
   $('#txtInput').bind('keypress set', null, watchTextbox);
 
-  // Example of JS code triggering the user event
-  $('#btnSetText').click(function (ev) {
-    $('#txtInput').val('abc def').trigger('set');
-  });
+  $(document).on("click", watchTextbox);
