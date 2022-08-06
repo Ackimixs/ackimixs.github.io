1526c6cb2e0c8a4bc8a9d62bad7f58473d9e49a7
Ackimixs <ackimixs@protonmail.com>
2022-08-06 23:26:05 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ackimixs.github.io
 
 website from tp for my school 
-
-IT'S JUST EXERCICE DON'T WORRY (whit little easter egg)
--- script/scriptSom.js
+++
@@ -1,6 +0,0 @@
-function soundOn() {
-    var ElonMusk = new Audio('../music/The Elon Musk Song [OFFICIAL].wav');
-    ElonMusk.play();
-}
-
-$( document ).on("click", "button", soundOn);
